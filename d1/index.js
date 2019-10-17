ec78991315cf89616714b0940b54e3088b7ea2b5
lucas <lucas.san.77@gmail.com>
2019-10-17 17:20:38 -0300

    Rotas adicionadas em index.js -  testes

--- d1/index.js
+++ d1/index.js
@@ -2,11 +2,67 @@ const express = require ('express');
 
 const server = express();
 
-server.use = (express.json());
+server.use(express.json());
 
 
+// array para armazenar os dados dos projetos
+const projects = [];
+
+// recebe um novo projeto ainda sem nenhuma tarefa
+server.post('/projects', (req, res) => {
+    const {id, title} = req.body;
+ 
+    const project = {
+      id,
+      title,
+      tasks: []
+    };
+
+    projects.push(project);
+  
+    return res.json(project);
+  });
+
+// Lista todos os projetos existentes
 server.get ('/projects',(req,res)=>{
-    return res.json({message: "working hello world"});
+    return res.json(projects);
+})
+
+// altera o titulo de um projeto atraves do id
+server.put('/projects/:id', (req,res)=>{
+    const {id} = req.params;
+    const {title}= req.body;
+
+    const project = projects.find(p => p.id == id);
+
+    project.title = title;
+
+    return res.json(project);
+
+})
+
+//deleta um projeto atraves do id
+server.delete('/projects/:id', (req,res)=>{
+    const {id} = req.params;
+    
+    const projectIndex =  projects.findIndex(p => p.id == id);
+
+    projects.splice(projectIndex,1);
+
+    return res.send();
+})
+
+// adiciona uma tarefa atraves do id do projeto
+
+server.put('/projects/:id/tasks', (req,res)=>{
+    const {id} = req.params;
+    const {title} = req.body;
+    
+    const project = projects.find(p => p.id == id);
+
+    project.tasks.push(title);
+
+    return res.json(project);
 })
 
 server.listen(3000);
